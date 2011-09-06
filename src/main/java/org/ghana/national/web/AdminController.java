23e9548ba87fa5afa11f51628bab850ff5fc4a06
Irfan Shah <mail.urfi@gmail.com>
2011-09-06 10:52:42 +0530

    Irfan/Puneet | #478 |  Spike Spring Security

---
+++ src/main/java/org/ghana/national/web/AdminController.java
@@ -0,0 +1,29 @@
+package org.ghana.national.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class AdminController {
+    private Logger log = Logger.getLogger("AdminController");
+
+    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
+    public void hello(HttpServletRequest request, HttpServletResponse response) {
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            outputStream.print("HEllo dude!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
