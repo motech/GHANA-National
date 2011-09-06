d2ff583d46c6aa1fce712923d290649fb6948775
Puneet Goyal <pungoyal@gmail.com>
2011-09-06 16:56:31 +0530

    pgoyal/irfn | #448 | Ensuring that a user exists in the database and has the correct role for it to be logged in.

--- src/main/java/org/ghana/national/dao/AllSuperAdmins.java
+++ src/main/java/org/ghana/national/dao/AllSuperAdmins.java
@@ -1,12 +1,16 @@
 package org.ghana.national.dao;
 
+import org.ektorp.ComplexKey;
 import org.ektorp.CouchDbConnector;
+import org.ektorp.support.View;
 import org.ghana.national.domain.SuperAdmin;
 import org.motechproject.dao.MotechAuditableRepository;
 import org.motechproject.security.SecurityGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class AllSuperAdmins extends MotechAuditableRepository<SuperAdmin> implements SecurityGroup {
@@ -16,14 +20,14 @@ public class AllSuperAdmins extends MotechAuditableRepository<SuperAdmin> implem
         super(SuperAdmin.class, db);
     }
 
-    public UserDetails getAuthenticatedUser(String username, String password) {
-        return null;
+    public UserDetails getAuthenticatedUser(String userName, String password) {
+        return findByUserNameAndPassword(userName, password);
     }
 
-    public SuperAdmin findByUserNameAndPassword(String userName, String password) {
-        String hashedPassword = password;
-
-
-        return new SuperAdmin();
+    @View(name = "find_by_user_name_and_password", map = "function(doc) {{emit([doc.username]);}}")
+    public SuperAdmin findByUserNameAndPassword(String username, String password) {
+        List<SuperAdmin> superAdmins = queryView("find_by_user_name_and_password", ComplexKey.of(username));
+        if (superAdmins.isEmpty()) return null;
+        return superAdmins.get(0);
     }
 }
--- src/main/java/org/ghana/national/domain/SuperAdmin.java
+++ src/main/java/org/ghana/national/domain/SuperAdmin.java
@@ -1,8 +1,89 @@
 package org.ghana.national.domain;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.ektorp.support.TypeDiscriminator;
 import org.motechproject.model.MotechAuditableDataObject;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
 
 @TypeDiscriminator("doc.type === 'SUPER_ADMIN'")
-public class SuperAdmin extends MotechAuditableDataObject {
+public class SuperAdmin extends MotechAuditableDataObject implements UserDetails {
+    @JsonProperty("type")
+    private final String type = "SUPER_ADMIN";
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+
+
+    private SuperAdmin() {
+    }
+
+    public SuperAdmin(String username) {
+        this.username = username;
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.isCredentialsNonExpired = true;
+        this.isEnabled = true;
+    }
+
+    @JsonIgnore
+    public Collection<GrantedAuthority> getAuthorities() {
+        GrantedAuthority grantedAuthority = new GrantedAuthority() {
+            public String getAuthority() {
+                return "admin";
+            }
+        };
+        return asList(grantedAuthority);
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        isCredentialsNonExpired = credentialsNonExpired;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
 }
