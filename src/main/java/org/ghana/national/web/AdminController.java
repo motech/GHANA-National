9239495dab423d84a08fdfc022cca310998fa2c3
Irfan Shah <mail.urfi@gmail.com>
2011-09-12 16:04:22 +0530

    Irfan/Avinash | #334 |  Basic Tiles configuration for layouts, added jquery

--- src/main/java/org/ghana/national/web/AdminController.java
+++ src/main/java/org/ghana/national/web/AdminController.java
@@ -3,11 +3,17 @@ package org.ghana.national.web;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/admin")
-public class AdminController extends DummyController{
+public class AdminController {
     private Logger log = Logger.getLogger("AdminController");
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String dashboard(){
+        return "admin/dashboard";
+    }
 }
 
 
