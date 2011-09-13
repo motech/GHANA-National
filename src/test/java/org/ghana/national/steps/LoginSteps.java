0851b74a90a9edb68b3b1a24806b31b7d1fb1817
Irfan Shah <mail.urfi@gmail.com>
2011-09-13 13:52:13 +0530

    irfn/avinash| #448 | Added a Sample story for admin login.

--- src/test/java/org/ghana/national/steps/LoginSteps.java
+++ src/test/java/org/ghana/national/steps/LoginSteps.java
@@ -1,6 +1,8 @@
 package org.ghana.national.steps;
 
 import org.ghana.national.pages.PageFactory;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
 
 public class LoginSteps {
     private PageFactory pageFactory;
@@ -8,4 +10,20 @@ public class LoginSteps {
     public LoginSteps(PageFactory pageFactory) {
         this.pageFactory = pageFactory;
     }
+
+    @Given("the $user user logs in with password $password")
+    public void userLogsIn(String user, String password) {
+
+    }
+
+    @Then("the $page page should be displayed")
+    public void displaysPage(String page) {
+
+    }
+
+    @Then("the $page page should be displayed with $error")
+    public void displaysPage(String page, String error) {
+
+    }
+
 }
--- src/test/resources/adminDashboard.story
+++ src/test/resources/adminDashboard.story
@@ -1,3 +1,19 @@
-Scenario: Admin logs in
-    Given the admin user logs in
-    Then the home page should be displayed
+Admin Login
+
+Meta:
+@category smoke
+
+Narrative: 
+
+As an Super Admin User I want to Login/Logout of the Web application So that I can access the application.
+
+Scenario: Successful Login
+
+Given the admin user logs in with password admin
+Then the admin dashboard page should be displayed
+
+Scenario: Failed Login
+
+Given the admin user logs in with password foo
+Then the admin dashboard page should be displayed with an error
+
