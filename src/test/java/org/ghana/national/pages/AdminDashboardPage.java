3953063870d1bd8e852a546d32e945d787278265
Puneet Goyal <pungoyal@gmail.com>
2011-09-15 13:52:59 +0530

    pungoyal | adding good file (with the right case)

---
+++ src/test/java/org/ghana/national/pages/AdminDashboardPage.java
@@ -0,0 +1,10 @@
+package org.ghana.national.pages;
+
+import org.jbehave.web.selenium.WebDriverProvider;
+
+public class AdminDashboardPage extends GhanaPage {
+    public AdminDashboardPage(WebDriverProvider driverProvider) {
+        super(driverProvider, "admin");
+    }
+
+}
