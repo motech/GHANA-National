ac76a191d46bf8768fe8566879c83b7abc64899e
Irfan Shah <mail.urfi@gmail.com>
2011-09-19 17:15:21 +0530

    irfn| #334| recreate and seed db for functional tests

---
+++ src/main/java/org/ghana/national/tools/CouchDB.java
@@ -0,0 +1,18 @@
+package org.ghana.national.tools;
+
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CouchDB {
+    @Autowired
+    private CouchDbInstance couchDbInstance;
+    @Autowired
+    private CouchDbConnector dbConnector;
+
+    public void recreate() {
+        String dbName = dbConnector.getDatabaseName();
+        couchDbInstance.deleteDatabase(dbName);
+        couchDbInstance.createDatabase(dbName);
+    }
+}
