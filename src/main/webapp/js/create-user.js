7ecf4a791414396e0641498f1cff4662f8f25c30
vijayaraj <vijayaraj@yahoo.com>
2011-10-25 13:09:33 +0530

    added regex for name input check <aussie/vijay>

--- src/main/webapp/js/create-user.js
+++ src/main/webapp/js/create-user.js
@@ -1,6 +1,7 @@
 $.UserFormValidator = function() {
     var email_regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
     var phone_regex = /^0[0-9]{9}$/;
+    var name_reg = /^\D+$/;
 
     var hideAllErrors = function() {
         $("#firstName_error").html('').hide();
@@ -20,15 +21,40 @@ $.UserFormValidator = function() {
             $("#firstName_error").html("Please enter first name");
             $("#firstName_error").show();
         }
+        if (!name_reg.test($("#user_first_name").val())) {
+            isValid = false;
+            $("#firstName_error").html("Please enter valid first name");
+            $("#firstName_error").show();
+        }
+
         if ($("#user_last_name").val().length == 0) {
             isValid = false;
             $("#lastName_error").html("Please enter last name");
             $("#lastName_error").show();
         }
+
+        if (!name_reg.test($("#user_last_name").val())) {
+            isValid = false;
+            $("#lastName_error").html("Please enter valid last name");
+            $("#lastName_error").show();
+        }
+
+        if ($("#user_middle_name").val().length != 0 && !name_reg.test($("#user_middle_name").val())) {
+            isValid = false;
+            $("#middleName_error").html("Please enter valid middle name");
+            $("#middleName_error").show();
+        }
+
         if ($("#user_phone").val().length == 0 || !phone_regex.test($("#user_phone").val())) {
             isValid = false;
             $("#phone_error").html("Please enter valid phone number");
             $("#phone_error").show();
+        }
+
+        if (!name_reg.test($("#user_first_name").val())) {
+            isValid = false;
+            $("#firstName_error").html("Please enter valid first name");
+            $("#firstName_error").show();
         }
 
         var role = $("#user_role option:selected").text();
