6e00f9d762dcf35b68e2f0024dd24131cbe8eca6
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-10-27 17:41:39 +0530

    Story #444: Moved populating facility fields logic into a separate file to reuse in Patients form

--- src/main/webapp/js/create-patient.js
+++ src/main/webapp/js/create-patient.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
     $("#createPatientForm").formly({'onBlur':false, 'theme':'Light'});
+    new Field('regions').hasADependent(new Field('districts').hasADependent(new Field('sub-districts')));
 });
