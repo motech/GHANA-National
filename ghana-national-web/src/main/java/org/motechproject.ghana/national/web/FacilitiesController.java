ca6d2098b9e79e4d6def6bc806d05271fed7afcc
vijayaraj <vijayaraj@yahoo.com>
2011-10-29 23:49:43 +0530

    made multi-modular -web,xforms,core #step1 <vijay>

---
+++ ghana-national-web/src/main/java/org/motechproject.ghana/national/web/FacilitiesController.java
@@ -0,0 +1,68 @@
+package org.motechproject.ghana.national.web;
+
+import org.motechproject.ghana.national.domain.Constants;
+import org.motechproject.ghana.national.exception.FacilityAlreadyFoundException;
+import org.motechproject.ghana.national.service.FacilityService;
+import org.motechproject.ghana.national.web.form.CreateFacilityForm;
+import org.motechproject.openmrs.advice.ApiSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.Locale;
+
+@Controller
+@RequestMapping(value = "/admin/facilities")
+public class FacilitiesController {
+    final static String SUCCESS = "facilities/success";
+    final static String NEW_FACILITY = "facilities/new";
+
+    private FacilityService facilityService;
+    private MessageSource messageSource;
+
+    public FacilitiesController() {
+    }
+
+    @Autowired
+    public FacilitiesController(FacilityService facilityService, MessageSource messageSource) {
+        this.facilityService = facilityService;
+        this.messageSource = messageSource;
+    }
+
+    @ApiSession
+    @RequestMapping(value = "new", method = RequestMethod.GET)
+    public String newFacilityForm(ModelMap modelMap) {
+        modelMap.put(Constants.CREATE_FACILITY_FORM, new CreateFacilityForm());
+        modelMap.mergeAttributes(facilityService.populateFacilityData());
+        return NEW_FACILITY;
+    }
+
+    @ApiSession
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String createFacility(@Valid CreateFacilityForm createFacilityForm, BindingResult bindingResult, ModelMap modelMap) {
+        try {
+            facilityService.create(createFacilityForm.getName(), createFacilityForm.getCountry(), createFacilityForm.getRegion(),
+                                    createFacilityForm.getCountyDistrict(), createFacilityForm.getStateProvince(), createFacilityForm.getPhoneNumber(),
+                                    createFacilityForm.getAdditionalPhoneNumber1(), createFacilityForm.getAdditionalPhoneNumber2(),
+                                    createFacilityForm.getAdditionalPhoneNumber3());
+            modelMap.mergeAttributes(facilityService.populateFacilityData());
+        } catch (FacilityAlreadyFoundException e) {
+            handleExistingFacilityError(bindingResult, modelMap, messageSource.getMessage("facility_already_exists", null, Locale.getDefault()));
+            return NEW_FACILITY;
+        }
+        return SUCCESS;
+    }
+
+    private void handleExistingFacilityError(BindingResult bindingResult, ModelMap modelMap, String message) {
+        modelMap.mergeAttributes(facilityService.populateFacilityData());
+        bindingResult.addError(new FieldError(Constants.CREATE_FACILITY_FORM, "name", message));
+        modelMap.mergeAttributes(bindingResult.getModel());
+    }
+
+}
---
+++ ghana-national-web/src/test/java/org/motechproject.ghana/national/web/security/LoginSuccessHandlerTest.java
@@ -0,0 +1,94 @@
+package org.motechproject.ghana.national.web.security;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.motechproject.mrs.security.MRSSecurityUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.WebAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+@Ignore
+public class LoginSuccessHandlerTest {
+    private HttpServletRequest servletRequest;
+    private Authentication authentication;
+    private MRSSecurityUser user;
+    private HttpSession httpSession;
+    private HttpServletResponse response;
+
+    @Before
+    public void setUp() {
+        servletRequest = mock(HttpServletRequest.class);
+        authentication = mock(Authentication.class);
+        user = mock(MRSSecurityUser.class);
+        httpSession = mock(HttpSession.class);
+        response = mock(HttpServletResponse.class);
+    }
+
+    @Test
+    public void shouldSetUserOnSessionOnAuthenticationSuccess() throws Exception {
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(servletRequest.getSession()).thenReturn(httpSession);
+
+        new LoginSuccessHandler().onAuthenticationSuccess(servletRequest, response, authentication);
+        verify(httpSession).setAttribute(LoginSuccessHandler.LOGGED_IN_USER, user);
+    }
+
+    @Test
+    public void shouldRedirectSystemDeveloperToAdminIfNoSavedRequest() throws Exception {
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(servletRequest.getSession()).thenReturn(httpSession);
+        when(user.getAuthorities()).thenReturn(new ArrayList<GrantedAuthority>(){{add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "System Developer";
+            }
+        });}});
+        when(httpSession.getAttribute(WebAttributes.SAVED_REQUEST)).thenReturn(null);
+        when(servletRequest.getSession(false)).thenReturn(httpSession);
+
+        new LoginSuccessHandler().onAuthenticationSuccess(servletRequest, response, authentication);
+        verify(response).sendRedirect("admin");
+    }
+
+    @Test
+    public void shouldRedirectCallcenterAdminToCallcenterIfNoSavedRequest() throws Exception {
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(servletRequest.getSession()).thenReturn(httpSession);
+        when(user.getAuthorities()).thenReturn(new ArrayList<GrantedAuthority>(){{add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "Create/Edit MoTeCH Data";
+            }
+        });}});
+        when(httpSession.getAttribute(WebAttributes.SAVED_REQUEST)).thenReturn(null);
+        when(servletRequest.getSession(false)).thenReturn(httpSession);
+
+        new LoginSuccessHandler().onAuthenticationSuccess(servletRequest, response, authentication);
+        verify(response).sendRedirect("callcenter");
+    }
+
+    @Test
+    public void shouldRedirectFacilityUserToFacilityIfNoSavedRequest() throws Exception {
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(servletRequest.getSession()).thenReturn(httpSession);
+        when(user.getAuthorities()).thenReturn(new ArrayList<GrantedAuthority>(){{add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "MoTeCH View Only";
+            }
+        });}});
+        when(httpSession.getAttribute(WebAttributes.SAVED_REQUEST)).thenReturn(null);
+        when(servletRequest.getSession(false)).thenReturn(httpSession);
+
+        new LoginSuccessHandler().onAuthenticationSuccess(servletRequest, response, authentication);
+        verify(response).sendRedirect("facility");
+    }
+}
