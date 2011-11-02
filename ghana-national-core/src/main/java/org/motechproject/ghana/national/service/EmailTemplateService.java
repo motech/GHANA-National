c0bb0a1d7d51ef28d2bf89f9e8b244e117599305
Khaarthi <khaarthi>
2011-11-02 17:28:35 +0530

    Geet/Khaarthi -- Email Template Service -- to be refactored

---
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/service/EmailTemplateService.java
@@ -0,0 +1,50 @@
+package org.motechproject.ghana.national.service;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.motechproject.ghana.national.domain.Email;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmailTemplateService {
+
+    private String motechFromAddress;
+    private String subjectTemplate;
+    private String textTemplate;
+
+    private VelocityEngine velocityEngine;
+    private EmailService emailService;
+
+    public EmailTemplateService(String motechFromAddress, String subjectTemplate, String textTemplate) {
+        this.motechFromAddress = motechFromAddress;
+        this.subjectTemplate = subjectTemplate;
+        this.textTemplate = textTemplate;
+    }
+
+    public void sendEmailUsingTemplates(String userName, String staffId, String password) {
+        final String toEmailId = userName;
+        final String staffIdentification = staffId;
+        final String defaultPassword = password;
+
+        Map emailData = new HashMap() {{
+            put("userName", toEmailId);
+            put("staffId", staffIdentification);
+            put("password", defaultPassword);
+
+        }};
+        emailService.send(new Email(userName, motechFromAddress, renderWith(subjectTemplate, emailData), renderWith(textTemplate, emailData)));
+    }
+
+    private String renderWith(String template, Map data) {
+        return VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, template, data).trim();
+    }
+
+    public void setVelocityEngine(VelocityEngine velocityEngine) {
+        this.velocityEngine = velocityEngine;
+    }
+
+    public void setEmailService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+}
---
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/service/EmailTemplateServiceIT.java
@@ -0,0 +1,31 @@
+package org.motechproject.ghana.national.service;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/testApplicationContext-core.xml"})
+public class EmailTemplateServiceIT extends AbstractJUnit4SpringContextTests {
+
+    @Autowired
+    private EmailTemplateService emailTemplateService;
+
+    @Test
+    @Ignore("This test is an integration test to be tested once all the smtp setup is done")
+    public void shouldSendEmailWithTemplates() {
+
+        final String toEmailId = "geetchan@thoughtworks.com";
+        final String staffId = "123456789";
+        final String password = "abcd1234";
+
+        emailTemplateService.sendEmailUsingTemplates(toEmailId, staffId, password);
+    }
+
+}
