eada9e5876e00c31e32a2e257dedabe4e6b8d715
Khaarthi <khaarthi>
2011-11-09 11:54:22 +0530

    Geet |khaarthi -- Refactored with motech email address

--- ghana-national-web/src/main/java/org/motechproject.ghana/national/web/ForgotPasswordController.java
+++ ghana-national-web/src/main/java/org/motechproject.ghana/national/web/ForgotPasswordController.java
@@ -3,6 +3,7 @@ package org.motechproject.ghana.national.web;
 import org.motechproject.ghana.national.domain.Constants;
 import org.motechproject.ghana.national.service.EmailTemplateService;
 import org.motechproject.ghana.national.service.UserService;
+import org.openmrs.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import org.openmrs.api.context.Context;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,20 +31,30 @@ public class ForgotPasswordController {
         this.emailTemplateService = emailTemplateService;
     }
 
-//    @ApiSession
     @RequestMapping(value = "/forgotPassword", method = RequestMethod.GET)
     public ModelAndView changePasswordAndSendEmail(HttpServletRequest request, ModelMap modelMap) throws ServletRequestBindingException {
         ModelAndView modelAndView = new ModelAndView("redirect:forgotPasswordStatus");
-        String emailId = request.getParameter("emailId");
-        String newPassword = userService.changePasswordByEmailId(emailId);
-        if (!newPassword.equals("")) {
-            String emailSentStatus = emailTemplateService.sendEmailUsingTemplates(emailId, newPassword);
-            if (emailSentStatus.equals(Constants.EMAIL_SUCCESS))
-                modelAndView.addObject("message", "Your Password is sent via email successfully");
-            else
-                modelAndView.addObject("message", "Your Password is not sent to you via email successfully.Please try again.");
-        } else {
-            modelAndView.addObject("message", "User Name not found. Please register");
+        try {
+            Context.openSession();
+            Context.authenticate("admin","P@ssw0rd");
+//            Context.getUserService().changePassword("sss","""");
+//            User authenticatedUser = Context.getAuthenticatedUser();
+//            Context.getUserContext().addProxyPrivilege();
+//            Context.becomeUser("admin");
+
+            String emailId = request.getParameter("emailId");
+            String newPassword = userService.changePasswordByEmailId(emailId);
+            if (!newPassword.equals("")) {
+                String emailSentStatus = emailTemplateService.sendEmailUsingTemplates(emailId, newPassword);
+                if (emailSentStatus.equals(Constants.EMAIL_SUCCESS))
+                    modelAndView.addObject("message", "Your Password is sent via email successfully");
+                else
+                    modelAndView.addObject("message", "Your Password is not sent to you via email successfully.Please try again.");
+            } else {
+                modelAndView.addObject("message", "User Name not found. Please register");
+            }
+        } finally {
+            Context.closeSession();
         }
         return modelAndView;
     }
