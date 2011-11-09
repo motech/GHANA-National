bd76b3faa18dee0ebe08dfbfd5994646f4f512d9
vijayaraj <vijayaraj@yahoo.com>
2011-11-09 09:52:29 +0530

    corrected generalqueryform <vijay>

--- ghana-national-xforms/src/main/java/org/motechproject/ghana/national/handlers/GeneralQueryFormHandler.java
+++ ghana-national-xforms/src/main/java/org/motechproject/ghana/national/handlers/GeneralQueryFormHandler.java
@@ -5,13 +5,15 @@ import org.motechproject.model.MotechEvent;
 import org.motechproject.server.event.annotations.MotechListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GeneralQueryFormHandler implements FormPublishHandler {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    @MotechListener(subjects = "form.validation.successful.General Query.NurseQuery")
+    @MotechListener(subjects = "form.validation.successful.NurseQuery.GeneralQuery")
     public void handleFormEvent(MotechEvent event) {
         log.info(event.getSubject() + "|params=" + event.getParameters());
     }
