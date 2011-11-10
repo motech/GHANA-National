33fbcecc67b74ea9640e46005e83228ca316fa9d
deepans <deepans@thoughtworks.com>
2011-11-10 16:02:37 +0530

    Deepan | Khaarthi - RegisterClientFormValidator as a component

--- ghana-national-xforms/src/main/java/org/motechproject/ghana/national/validator/RegisterClientFormValidator.java
+++ ghana-national-xforms/src/main/java/org/motechproject/ghana/national/validator/RegisterClientFormValidator.java
@@ -3,9 +3,10 @@ package org.motechproject.ghana.national.validator;
 import org.motechproject.mobileforms.api.domain.FormBean;
 import org.motechproject.mobileforms.api.domain.FormError;
 import org.motechproject.mobileforms.api.validator.FormValidator;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public class RegisterClientFormValidator extends FormValidator {
     @Override
     public List<FormError> validate(FormBean formBean) {
