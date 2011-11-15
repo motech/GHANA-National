d653d7d57ec312058949d9fb6434e9dc351f93d6
vijayaraj <vijayaraj@yahoo.com>
2011-11-15 15:30:11 +0530

    Vijay/Khaarthi moved email configuration

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/service/IdentifierService.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/service/IdentifierService.java
@@ -1,9 +1,18 @@
 package org.motechproject.ghana.national.service;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class IdentifierService {
+    private Logger log = Logger.getLogger(IdentifierService.class.getName());
+
+//    @Value("#{mobileFormsProperties['forms.xml.form.name']}")
+//    private String url;
+
+
 
 }
