e1ee112cac99f8678c88f776efece47830f08840
vijayaraj <vijayaraj@yahoo.com>
2011-11-15 14:12:01 +0530

    #826 removed old spike identifier implementation <khaarthiga/vijay>

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/service/IdentifierService.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/service/IdentifierService.java
@@ -1,23 +1,9 @@
 package org.motechproject.ghana.national.service;
 
 
-import org.motechproject.mrs.services.MRSIdentifierAdaptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IdentifierService {
-    private MRSIdentifierAdaptor identifierAdaptor;
 
-    public IdentifierService() {
-    }
-
-    @Autowired
-    public IdentifierService(MRSIdentifierAdaptor identifierAdaptor) {
-        this.identifierAdaptor = identifierAdaptor;
-    }
-
-    public MRSIdentifierAdaptor getIdentifierAdaptor() {
-        return identifierAdaptor;
-    }
 }
