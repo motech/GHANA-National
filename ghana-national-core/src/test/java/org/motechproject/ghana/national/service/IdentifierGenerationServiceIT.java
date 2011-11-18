b473a739fc832cb98bf9f41ff884939f07ac5710
Khaarthi <khaarthi>
2011-11-18 10:31:54 +0530

    Khaarthi - Fixed Build

--- ghana-national-core/src/test/java/org/motechproject/ghana/national/service/IdentifierGenerationServiceIT.java
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/service/IdentifierGenerationServiceIT.java
@@ -1,6 +1,7 @@
 package org.motechproject.ghana.national.service;
 
 import org.apache.commons.lang.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ public class IdentifierGenerationServiceIT {
     private IdentifierGenerationService identifierService;
 
     @Test
-//    @Ignore("just to test and verify with omod up and running")
+    @Ignore("just to test and verify with omod up and running")
     public void shouldGenerateIDForAnIDTypeAndGenerator() throws IOException {
 
         String facilityId = identifierService.newFacilityId();
