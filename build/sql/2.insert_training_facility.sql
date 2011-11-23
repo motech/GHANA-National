321ce53f22c98fd74fec7ccef3dac9654bf919ee
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-11-23 11:36:54 +0530

    Added training facility as per David's request <aus/balaji>

---
+++ build/sql/2.insert_training_facility.sql
@@ -0,0 +1,2 @@
+insert into location (name, country, county_district, address6, creator, uuid)
+values ('Training Facility', 'Ghana', 'Training District', 'Training Region', 1, '8e1f5aa2-4476-413a-90e3-73ba891db62d');
