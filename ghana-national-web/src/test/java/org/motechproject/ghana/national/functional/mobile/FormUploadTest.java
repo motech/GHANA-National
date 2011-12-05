d038a8ed7f31412cdc9da5d77da654ef18b86d4b
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-12-05 16:44:20 +0530

    introducing functional test layer for mobile form uploads <balaji/khaarthi>

--- ghana-national-web/src/test/java/org/motechproject/ghana/national/functional/mobile/FormUploadTest.java
+++ ghana-national-web/src/test/java/org/motechproject/ghana/national/functional/mobile/FormUploadTest.java
@@ -1,24 +1,53 @@
 package org.motechproject.ghana.national.functional.mobile;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.fcitmuk.epihandy.EpihandyXformSerializer;
+import org.fcitmuk.epihandy.midp.db.FormDataStore;
+import org.fcitmuk.epihandy.midp.db.StudyStore;
+import org.fcitmuk.epihandy.midp.model.Model;
+import org.fcitmuk.epihandy.midp.transport.FormUpload;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
+@Ignore
 public class FormUploadTest {
     @Test
     public void shouldDownloadDefaultXFormsFromGhanaNational() {
-        final HttpClient httpClient = new HttpClient();
         try {
-            final PostMethod postMethod = new PostMethod("http://localhost:8080/ghana-national-web/formdownload");
-            httpClient.executeMethod(postMethod);
-            final InputStream responseStream = postMethod.getResponseBodyAsStream();
-            EpihandyXformSerializer serializer = new EpihandyXformSerializer();
+            final HttpConnection httpConnection = (HttpConnection) Connector.open("http://localhost:8080/ghana-national-web/formupload");
+            httpConnection.setRequestMethod(HttpConnection.POST);
+            httpConnection.setRequestProperty("Content-Type", "application/octet-stream");
+            httpConnection.setRequestProperty("User-Agent", "Profile/MIDP-2.0 Configuration/CLDC-1.0");
+            httpConnection.setRequestProperty("Content-Language", "en-US");
+
+            DataOutputStream dataOutputStream = httpConnection.openDataOutputStream();
+            dataOutputStream.writeUTF("motech");
+            dataOutputStream.writeUTF("ghs");
+            dataOutputStream.writeUTF("");
+            dataOutputStream.writeUTF("");
+            dataOutputStream.writeByte(2);
+            final StudyStore studyStore = new StudyStore();
+            final FormDataStore formDataStore = new FormDataStore();
+//            formDataStore.saveFormData()
+            FormUpload formUpload = new FormUpload(new Model(studyStore, formDataStore));
+            formUpload.write(dataOutputStream);
+
+            if (httpConnection.getResponseCode() == HttpConnection.HTTP_OK) {
+                System.out.println("success");
+                final DataInputStream dataInputStream = httpConnection.openDataInputStream();
+                assertResponse(dataInputStream);
+            }
+
         } catch (IOException e) {
             System.out.println(e);
         }
+    }
+
+    private void assertResponse(DataInputStream dataInputStream) {
+
     }
 }
