a540bffa64402b3cdd894f874f99d3f3c01f2388
Geet Chandratre <geetchandratre@gmail.com>
2011-12-26 11:01:52 +0530

    Geet | #336 | Added title to the search patient page

--- ghana-national-web/src/main/webapp/js/search-patient.js
+++ ghana-national-web/src/main/webapp/js/search-patient.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $("#searchPatientForm").formly({'onBlur':false, 'theme':'Light'});
 
     var validate = function(form) {
         formValidator.clearMessages(form);
