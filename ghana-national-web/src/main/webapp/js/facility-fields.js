729c4565aa34a5348fec2a21dcf4833b25fd6db0
Arvind Kumar C <arvindc@thoughtworks.com>
2012-01-03 18:45:44 +0530

    Revert "Khaarthiga, Arvind | Fixed issue with sub-district display in edit-facility and edit-patient page"
    This reverts commit 51f9eb136b02ce67426e468b5d262b0a159a3396.

--- ghana-national-web/src/main/webapp/js/facility-fields.js
+++ ghana-national-web/src/main/webapp/js/facility-fields.js
@@ -13,14 +13,12 @@ var facilities = (function(){
             }
             if(element.find('option:selected').attr('parent') != 'select') {
                 facilities.initializeSelectBoxWithTheCompleteFacilityList();
-                var facilitySelect = $('#facilities');
-                var selectedFacility = facilitySelect.find('option:selected').val();
-                facilitySelect.html($('#facilities').find('option').filter(function() {
+                $('#facilities').html($('#facilities').find('option').filter(function() {
                     return ($(this).attr('parent') == element.find('option:selected').text());
                 }));
-                facilitySelect.prepend('<option value="" parent="select">Select Facility</option>');
-                facilitySelect.val(selectedFacility);
-                facilitySelect.parent().show();
+                $('#facilities').prepend('<option value="" parent="select">Select Facility</option>');
+                $('#facilities').find('option:first').html('Select Facility');
+                $('#facilities').parent().show();
             }
         },
         hide :  function (field) {
@@ -88,7 +86,7 @@ Field.prototype.hideDependents = function() {
     while (field.hasDependent()) {
         var field = field.dependent;
         field.node.parent().hide();
-//        field.node.find('option:first').attr('selected', 'selected');
+        field.node.find('option:first').attr('selected', 'selected');
     }
 }
 
