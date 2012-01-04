ef5659c638029371ffa893177bec76e461693e15
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-01-04 10:17:14 +0530

    <balaji/aussie>|#756|deleted test xml files.

--- ghana-national-web/src/test/java/org/motechproject/ghana/national/functional/mobile/RegisterANCMobileUploadTest.java
+++ ghana-national-web/src/test/java/org/motechproject/ghana/national/functional/mobile/RegisterANCMobileUploadTest.java
@@ -8,9 +8,9 @@ import java.util.List;
 import static org.testng.Assert.assertEquals;
 
 public class RegisterANCMobileUploadTest {
+
     @Test
     public void shouldCheckForMandatoryFields() throws Exception {
-
         final XformHttpClient.XformResponse xformResponse = XformHttpClient.execute("http://localhost:8080/ghana-national-web/formupload",
                 "NurseDataEntry", XFormParser.parse("register-anc-template.xml", MapUtils.EMPTY_MAP));
         final List<XformHttpClient.Error> errors = xformResponse.getErrors();
