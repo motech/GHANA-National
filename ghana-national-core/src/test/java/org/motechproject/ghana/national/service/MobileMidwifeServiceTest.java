6688fbd1aa2954c4df68c3511e9c56380ae5ccb4
Khaarthi <khaarthi>
2012-01-11 18:45:53 +0530

    Khaarthi/Alex | Mobile Midwife changes

---
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/service/MobileMidwifeServiceTest.java
@@ -0,0 +1,31 @@
+package org.motechproject.ghana.national.service;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.motechproject.ghana.national.builder.MobileMidwifeEnrollmentBuilder;
+import org.motechproject.ghana.national.domain.mobilemidwife.MobileMidwifeEnrollment;
+import org.motechproject.ghana.national.repository.AllMobileMidwifeEnrollments;
+import org.motechproject.model.DayOfWeek;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class MobileMidwifeServiceTest {
+    @Mock
+    private AllMobileMidwifeEnrollments enrollments;
+    private MobileMidwifeService service;
+
+    public MobileMidwifeServiceTest() {
+        initMocks(this);
+        service = new MobileMidwifeService();
+    }
+
+    @Test
+    public void shouldSaveMobileMidwifeEnrollment() {
+        MobileMidwifeEnrollment enrollment = new MobileMidwifeEnrollmentBuilder().facilityId("facility12").
+                patientId("patientId").staffId("staff13").consent(true).dayOfWeek(DayOfWeek.Thursday)
+                .build();
+        service.save(enrollment);
+        service.findBy(enrollment.getId());
+    }
+
+}
