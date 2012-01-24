6783b15b3b08fdd4d91575ac0f233a742bec6980
Arvind Kumar C <arvindc@thoughtworks.com>
2012-01-24 14:35:09 +0530

    Arvind, Geet | Fixed duplicate facility issue (for the same phone number)

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/exception/FacilityAlreadyFoundException.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/exception/FacilityAlreadyFoundException.java
@@ -3,4 +3,8 @@ package org.motechproject.ghana.national.exception;
 public class FacilityAlreadyFoundException extends Exception {
     public FacilityAlreadyFoundException() {
     }
+
+    public FacilityAlreadyFoundException(String message) {
+        super(message);
+    }
 }
