6f113438769f24cfa0ccd17709804a731787878f
Alex Anto Navis L <alexal@thoughtworks.com>
2012-02-15 14:34:53 +0530

    Alex, Khaarthi | #1204 | Made it abstract

--- ghana-national-core/src/test/java/org/motechproject/ghana/national/configuration/BaseScheduleTrackingIT.java
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/configuration/BaseScheduleTrackingIT.java
@@ -28,7 +28,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-public class BaseScheduleTrackingIT extends BaseUnitTest {
+public abstract class BaseScheduleTrackingIT extends BaseUnitTest {
 
     @Autowired
     private AllTrackedSchedules allTrackedSchedules;
