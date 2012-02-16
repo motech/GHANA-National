93bbb83190c6a9f322bfdb8e44679d279dfeb7e2
Alex Anto Navis L <alexal@thoughtworks.com>
2012-02-16 20:06:27 +0530

    Alex | #1204 | Added test for IPTVaccine story.
    - start week 12
    - start week 13.
    
    Since reference date couldnot be in the past.

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/configuration/CareScheduleNames.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/configuration/CareScheduleNames.java
@@ -3,5 +3,6 @@ package org.motechproject.ghana.national.configuration;
 public class CareScheduleNames {
     public static final String DELIVERY = "Delivery";
     public static final String TTVaccine = "TTVaccine";
-    public static final String ANC_IPT_VACCINE = "ANCIPTVaccine";
+    public static final String ANC_IPT_VACCINE_START_WEEK_12 = "ANCIPTVaccine_StartWeek12";
+    public static final String ANC_IPT_VACCINE_START_WEEK_13 = "ANCIPTVaccine_StartWeek13";
 }
