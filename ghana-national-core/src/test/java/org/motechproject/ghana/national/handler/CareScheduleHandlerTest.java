961878404e1ccdd03bc1af6a97b0191657511b87
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-02-24 16:01:47 +0530

    Balaji/Aus - fixing tests as a result of changes in platform

--- ghana-national-core/src/test/java/org/motechproject/ghana/national/handler/CareScheduleHandlerTest.java
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/handler/CareScheduleHandlerTest.java
@@ -9,11 +9,11 @@ import org.motechproject.scheduletracking.api.domain.Milestone;
 import org.motechproject.scheduletracking.api.domain.MilestoneAlert;
 import org.motechproject.scheduletracking.api.events.MilestoneEvent;
 import org.motechproject.util.DateUtil;
-import org.motechproject.valueobjects.WallTime;
 
+import static org.joda.time.Period.weeks;
 import static org.mockito.Mockito.*;
-import static org.motechproject.ghana.national.handler.CareScheduleHandler.*;
-import static org.motechproject.valueobjects.WallTimeUnit.Week;
+import static org.motechproject.ghana.national.handler.CareScheduleHandler.PREGNANCY_ALERT_SMS_KEY;
+import static org.motechproject.ghana.national.handler.CareScheduleHandler.TT_VACCINATION_SMS_KEY;
 
 public class CareScheduleHandlerTest {
 
@@ -41,7 +41,7 @@ public class CareScheduleHandlerTest {
         doNothing().when(careScheduleHandlerSpy).sendSMSToFacility(Matchers.<String>any(), Matchers.<MilestoneEvent>any(), Matchers.<LocalDate>any());
 
         LocalDate referenceDate = DateUtil.newDate(2012, 2, 1);
-        Milestone milestone = new Milestone("M1", new WallTime(0, Week), new WallTime(3, Week), new WallTime(6, Week), new WallTime(7, Week));
+        Milestone milestone = new Milestone("M1", weeks(0), weeks(3), weeks(6), weeks(7));
         final MilestoneEvent milestoneEvent = new MilestoneEvent(null, null, MilestoneAlert.fromMilestone(milestone, referenceDate), null, referenceDate);
 
         careScheduleHandlerSpy.handleTTVaccinationAlert(milestoneEvent);
