2e25e73266df7e153205ccf9aa7f64067f458a4a
Alex Anto Navis L <alexal@thoughtworks.com>
2012-02-29 16:56:29 +0530

    Alex | #347 | Created Measles schedule for CWC and its tests.

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/configuration/ScheduleNames.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/configuration/ScheduleNames.java
@@ -3,6 +3,7 @@ package org.motechproject.ghana.national.configuration;
 public class ScheduleNames {
     public static final String DELIVERY = "Delivery";
     public static final String ANC_IPT_VACCINE = "ANCIPTVaccine";
+    public static final String CWC_MEASLES_VACCINE = "CWCMeaslesVaccine";
     public static final String CWC_IPT_VACCINE = "CWCIPTVaccine";
     public static final String TT_VACCINATION_VISIT = "TTVaccine";
     public static final String BCG = "BCG";
