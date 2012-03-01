e08512e35cfa3b6f81ff992304d4dc863e644184
deepans <deepans@thoughtworks.com>
2012-03-01 11:51:38 +0530

    Deepan | #472 | Fixed nearest monday logic to always set time as 0 h, 0 m

--- ghana-national-messaging-gateway/src/main/java/org/motechproject/ghana/national/messagegateway/domain/NextMondayDispatcher.java
+++ ghana-national-messaging-gateway/src/main/java/org/motechproject/ghana/national/messagegateway/domain/NextMondayDispatcher.java
@@ -2,10 +2,12 @@ package org.motechproject.ghana.national.messagegateway.domain;
 
 import org.joda.time.DateTimeConstants;
 import org.joda.time.LocalDateTime;
+import org.motechproject.model.Time;
+import org.motechproject.util.DateUtil;
 
 public class NextMondayDispatcher implements DeliveryStrategy{
     @Override
     public LocalDateTime deliveryDate(SMS sms) {
-        return sms.getGenerationTime().plusWeeks(1).withDayOfWeek(DateTimeConstants.MONDAY);
+        return DateUtil.newDateTime(sms.getGenerationTime().plusWeeks(1).withDayOfWeek(DateTimeConstants.MONDAY).toLocalDate(), new Time(0, 0)).toLocalDateTime();
     }
 }
