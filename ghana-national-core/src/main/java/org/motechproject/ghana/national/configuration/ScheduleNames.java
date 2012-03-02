726c720b84d2b722d8c4d03fdc6324a5542657ea
Arvind Kumar C <arvindc@thoughtworks.com>
2012-03-02 11:58:10 +0530

    Arvind | Maintaining consistency in Schedule Names

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/configuration/ScheduleNames.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/configuration/ScheduleNames.java
@@ -5,9 +5,9 @@ public class ScheduleNames {
     public static final String ANC_IPT_VACCINE = "ANCIPTVaccine";
     public static final String CWC_MEASLES_VACCINE = "CWCMeaslesVaccine";
     public static final String CWC_IPT_VACCINE = "CWCIPTVaccine";
-    public static final String CWC_BCG = "BCG";
-    public static final String CWC_YELLOW_FEVER = "YellowFeverVaccine";
-    public static final String CWC_PENTA = "Penta";
+    public static final String CWC_BCG = "CWCBCGVaccine";
+    public static final String CWC_YELLOW_FEVER = "CWCYellowFeverVaccine";
+    public static final String CWC_PENTA = "CWCPentaVaccine";
     public static final String TT_VACCINATION_VISIT = "TTVaccine";
-    public static final String OPV_0 = "OPV-0";
+    public static final String OPV_0 = "CWCOPV-0";
 }
