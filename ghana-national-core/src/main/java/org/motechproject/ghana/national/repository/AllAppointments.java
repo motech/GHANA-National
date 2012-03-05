de3e0b916969bdd7aae060627f691c02b461108c
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-03-05 11:12:09 +0530

    Balaji - moving to 0.9 snapshot of motech platform and commenting out AllAppointments repo. wip

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/repository/AllAppointments.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/repository/AllAppointments.java
@@ -1,11 +1,7 @@
 package org.motechproject.ghana.national.repository;
 
 import org.joda.time.DateTime;
-import org.motechproject.appointments.api.contract.AppointmentCalendarRequest;
-import org.motechproject.appointments.api.contract.ReminderConfiguration;
-import org.motechproject.appointments.api.contract.VisitRequest;
-import org.motechproject.appointments.api.service.AppointmentService;
-import org.motechproject.ghana.national.domain.ANCVisitAppointment;
+import org.motechproject.appointments.api.contract.AppointmentService;
 import org.motechproject.ghana.national.domain.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -20,26 +16,26 @@ public class AllAppointments {
     }
 
     public void updateANCVisitSchedule(Patient patient, DateTime dueDate) {
-        remove(patient);
-        final ANCVisitAppointment ancVisitAppointment = ANCVisitAppointment.createFor(dueDate);
-        AppointmentCalendarRequest calendarRequest = new AppointmentCalendarRequest();
-        calendarRequest.setExternalId(patient.getMotechId());
-        appointmentService.addCalendar(calendarRequest);
-
-        final ReminderConfiguration configuration = new ReminderConfiguration();
-        configuration.setIntervalUnit(ReminderConfiguration.IntervalUnit.valueOf(ancVisitAppointment.getReminderInterval()));
-        configuration.setIntervalCount(ancVisitAppointment.getReminderCount());
-        appointmentService.addVisit(patient.getMotechId(), "DueVisit - " + patient.getMotechId(), new VisitRequest() {{
-            setDueDate(ancVisitAppointment.getDue());
-            setReminderConfiguration(configuration);
-        }});
-        appointmentService.addVisit(patient.getMotechId(), "LateVisit - " + patient.getMotechId(), new VisitRequest() {{
-            setDueDate(ancVisitAppointment.getLate());
-            setReminderConfiguration(configuration);
-        }});
-        appointmentService.addVisit(patient.getMotechId(), "MaxVisit - " + patient.getMotechId(), new VisitRequest() {{
-            setDueDate(ancVisitAppointment.getMax());
-            setReminderConfiguration(configuration);
-        }});
+//        remove(patient);
+//        final ANCVisitAppointment ancVisitAppointment = ANCVisitAppointment.createFor(dueDate);
+//        AppointmentCalendarRequest calendarRequest = new AppointmentCalendarRequest();
+//        calendarRequest.setExternalId(patient.getMotechId());
+//        appointmentService.addCalendar(calendarRequest);
+//
+//        final ReminderConfiguration configuration = new ReminderConfiguration();
+//        configuration.setIntervalUnit(ReminderConfiguration.IntervalUnit.valueOf(ancVisitAppointment.getReminderInterval()));
+//        configuration.setIntervalCount(ancVisitAppointment.getReminderCount());
+//        appointmentService.addVisit(patient.getMotechId(), "DueVisit - " + patient.getMotechId(), new VisitRequest() {{
+//            setDueDate(ancVisitAppointment.getDue());
+//            setReminderConfiguration(configuration);
+//        }});
+//        appointmentService.addVisit(patient.getMotechId(), "LateVisit - " + patient.getMotechId(), new VisitRequest() {{
+//            setDueDate(ancVisitAppointment.getLate());
+//            setReminderConfiguration(configuration);
+//        }});
+//        appointmentService.addVisit(patient.getMotechId(), "MaxVisit - " + patient.getMotechId(), new VisitRequest() {{
+//            setDueDate(ancVisitAppointment.getMax());
+//            setReminderConfiguration(configuration);
+//        }});
     }
 }
