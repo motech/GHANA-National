3b492eb6d384b64d545f00587a44689394979ea1
deepans <deepans@thoughtworks.com>
2012-03-06 14:35:57 +0530

    Deepan: #777, Migrated OPV schedules

---
+++ ghana-national-tools/src/main/java/org/motechproject/ghana/national/tools/seed/data/OPVSeed.java
@@ -0,0 +1,42 @@
+package org.motechproject.ghana.national.tools.seed.data;
+
+import org.motechproject.ghana.national.configuration.ScheduleNames;
+import org.motechproject.ghana.national.repository.AllSchedules;
+import org.motechproject.ghana.national.tools.seed.data.domain.UpcomingSchedule;
+import org.motechproject.ghana.national.tools.seed.data.source.OldGhanaScheduleSource;
+import org.motechproject.scheduletracking.api.repository.AllTrackedSchedules;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.lang.Boolean.TRUE;
+
+@Component("opvSeed")
+public class OPVSeed extends ScheduleMigrationSeed {
+
+    @Autowired
+    public OPVSeed(OldGhanaScheduleSource oldGhanaScheduleSource, AllTrackedSchedules allTrackedSchedules, AllSchedules allSchedules) {
+        super(allTrackedSchedules, oldGhanaScheduleSource, allSchedules, TRUE);
+    }
+
+    protected List<UpcomingSchedule> getAllUpcomingSchedules() {
+        return oldGhanaScheduleSource.getUpcomingOPVSchedules();
+    }
+
+
+    @Override
+    protected String mapMilestoneName(String milestoneName) {
+        return milestoneName;
+    }
+
+    @Override
+    public String getScheduleName(String milestoneName) {
+        if ("OPV0".equals(milestoneName)){
+            return ScheduleNames.CWC_OPV_0;
+        }else if ("OPV1".equals(milestoneName) || "OPV2".equals(milestoneName) || "OPV3".equals(milestoneName)){
+            return ScheduleNames.CWC_OPV_OTHERS;
+        }
+        return null;
+    }
+}
