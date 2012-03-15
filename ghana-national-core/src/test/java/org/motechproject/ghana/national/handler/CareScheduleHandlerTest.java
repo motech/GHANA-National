3f9c5c599ad112afd8869f5455e15f64a27ec6b9
Arvind Kumar C <arvindc@thoughtworks.com>
2012-03-15 07:50:54 +0530

    Arvind/Khaarthi | #353 | Added schedules to PNCMother

--- ghana-national-core/src/test/java/org/motechproject/ghana/national/handler/CareScheduleHandlerTest.java
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/handler/CareScheduleHandlerTest.java
@@ -3,18 +3,39 @@ package org.motechproject.ghana.national.handler;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.motechproject.ghana.national.domain.Facility;
+import org.motechproject.ghana.national.domain.Patient;
+import org.motechproject.ghana.national.repository.AllFacilities;
+import org.motechproject.ghana.national.repository.AllPatients;
+import org.motechproject.ghana.national.repository.SMSGateway;
+import org.motechproject.mrs.model.MRSFacility;
+import org.motechproject.mrs.model.MRSPatient;
+import org.motechproject.scheduletracking.api.domain.MilestoneAlert;
+import org.motechproject.scheduletracking.api.domain.WindowName;
 import org.motechproject.scheduletracking.api.events.MilestoneEvent;
+import org.motechproject.util.DateUtil;
+
+import java.util.Map;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.motechproject.ghana.national.domain.SmsTemplateKeys.*;
 
 public class CareScheduleHandlerTest {
 
     private CareScheduleHandler careScheduleHandlerSpy;
+    @Mock
+    private SMSGateway mockSmsGateway;
+    @Mock
+    private AllPatients mockAllPatients;
+    @Mock
+    private AllFacilities mockAllFacilities;
 
     @Before
     public void setUp() throws Exception {
-        careScheduleHandlerSpy = spy(new CareScheduleHandler());
+        initMocks(this);
+        careScheduleHandlerSpy = spy(new CareScheduleHandler(mockAllPatients, mockAllFacilities, mockSmsGateway));
         doNothing().when(careScheduleHandlerSpy).sendSMSToFacility(Matchers.<String>any(), Matchers.<MilestoneEvent>any());
     }
 
@@ -29,7 +50,7 @@ public class CareScheduleHandlerTest {
     @Test
     public void shouldHandleTTVaccinationAlert() {
 
-         final MilestoneEvent milestoneEvent = mock(MilestoneEvent.class);
+        final MilestoneEvent milestoneEvent = mock(MilestoneEvent.class);
         careScheduleHandlerSpy.handleTTVaccinationAlert(milestoneEvent);
         verify(careScheduleHandlerSpy).sendSMSToFacility(TT_VACCINATION_SMS_KEY, milestoneEvent);
     }
@@ -84,9 +105,28 @@ public class CareScheduleHandlerTest {
 
     @Test
     public void handlePentaVaccinationAlert() {
-
         final MilestoneEvent milestoneEvent = mock(MilestoneEvent.class);
         careScheduleHandlerSpy.handlePentaVaccinationAlert(milestoneEvent);
         verify(careScheduleHandlerSpy).sendSMSToFacility(CWC_PENTA_SMS_KEY, milestoneEvent);
+    }
+
+    @Test
+    public void handlePNCMotherAlert() {
+        MilestoneAlert milestoneAlert = mock(MilestoneAlert.class);
+        final MilestoneEvent milestoneEvent = new MilestoneEvent("1", "scheduleName", milestoneAlert, WindowName.due.name(), DateUtil.now());
+        Patient mockPatient = mock(Patient.class);
+        MRSPatient mockMRSPatient = mock(MRSPatient.class);
+        Facility facility = mock(Facility.class);
+        MRSFacility mrsFacility = mock(MRSFacility.class);
+        String facilityId = "facilityId";
+
+        when(mockPatient.getMrsPatient()).thenReturn(mockMRSPatient);
+        when(mockMRSPatient.getFacility()).thenReturn(mrsFacility);
+        when(mrsFacility.getId()).thenReturn(facilityId);
+        when(mockAllPatients.patientByOpenmrsId(milestoneEvent.getExternalId())).thenReturn(mockPatient);
+        when(mockAllFacilities.getFacility(facilityId)).thenReturn(facility);
+
+        careScheduleHandlerSpy.handlePNCMotherAlert(milestoneEvent);
+        verify(mockSmsGateway).dispatchSMS(eq(PNC_MOTHER_SMS_KEY), (Map<String, String>) any(), anyString());
     }
 }
