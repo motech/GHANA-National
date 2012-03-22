525e9a889c743ecdfdbcdf33dc6a89865a93e856
isha tripathi <ishatrip@thoughtworks.com>
2012-03-22 15:41:15 +0530

    Isha/Alex - checking in general query form,handler,validator

--- ghana-national-xforms/src/main/java/org/motechproject/ghana/national/validator/GeneralQueryFormValidator.java
+++ ghana-national-xforms/src/main/java/org/motechproject/ghana/national/validator/GeneralQueryFormValidator.java
@@ -1,17 +1,28 @@
 package org.motechproject.ghana.national.validator;
 
-import org.motechproject.mobileforms.api.domain.FormBean;
+import org.motechproject.ghana.national.bean.GeneralQueryForm;
 import org.motechproject.mobileforms.api.domain.FormError;
 import org.motechproject.mobileforms.api.validator.FormValidator;
+import org.motechproject.openmrs.advice.ApiSession;
+import org.motechproject.openmrs.advice.LoginAsAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
 
 @Component
-public class GeneralQueryFormValidator extends FormValidator {
+public class GeneralQueryFormValidator extends FormValidator<GeneralQueryForm> {
+
+    @Autowired
+    private org.motechproject.ghana.national.validator.FormValidator formValidator;
+
+    @LoginAsAdmin
+    @ApiSession
     @Override
-    public List<FormError> validate(FormBean formBean) {
-        return Collections.EMPTY_LIST;
+    public List<FormError> validate(GeneralQueryForm generalQueryForm) {
+        List<FormError> formErrors = super.validate(generalQueryForm);
+        formErrors.addAll(formValidator.validateIfFacilityExists(generalQueryForm.getFacilityId()));
+        formErrors.addAll(formValidator.validateIfStaffExists(generalQueryForm.getStaffId()));
+        return formErrors;
     }
 }
---
+++ ghana-national-xforms/src/test/java/org/motechproject/ghana/national/validator/GeneralQueryFormValidatorTest.java
@@ -0,0 +1,57 @@
+package org.motechproject.ghana.national.validator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.motechproject.ghana.national.bean.GeneralQueryForm;
+import org.motechproject.mobileforms.api.domain.FormError;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.motechproject.ghana.national.domain.Constants.NOT_FOUND;
+
+
+public class GeneralQueryFormValidatorTest {
+
+    private GeneralQueryFormValidator generalQueryFormValidator;
+    @Mock
+    private FormValidator formValidator;
+    @Before
+    public void setup(){
+        initMocks(this);
+        generalQueryFormValidator=new GeneralQueryFormValidator();
+        ReflectionTestUtils.setField(generalQueryFormValidator, "formValidator", formValidator);
+    }
+    
+    @Test
+    public void shouldValidateTheGeneralQueryFormBean() {
+        String facilityId = "13161";
+        String staffId = "23";
+        String responsePhoneNumber="0987654321";
+        String queryType="ANC defaulters";
+        GeneralQueryForm generalQueryForm = createGeneralQueryForm(facilityId, staffId, responsePhoneNumber, queryType);
+
+        when(formValidator.validateIfFacilityExists(facilityId)).thenReturn(asList(new FormError(facilityId, NOT_FOUND)));
+        when(formValidator.validateIfStaffExists(staffId)).thenReturn(asList(new FormError(staffId, NOT_FOUND)));
+
+        List<FormError> formErrors = generalQueryFormValidator.validate(generalQueryForm);
+
+        assertThat(formErrors, hasItem(new FormError(facilityId, NOT_FOUND)));
+        assertThat(formErrors, hasItem(new FormError(staffId, NOT_FOUND)));
+    }
+
+    private GeneralQueryForm createGeneralQueryForm(String facilityId, String staffId, String responsePhoneNumber,String queryType) {
+        GeneralQueryForm generalQueryForm = new GeneralQueryForm();
+        generalQueryForm.setFacilityId(facilityId);
+        generalQueryForm.setStaffId(staffId);
+        generalQueryForm.setResponsePhoneNumber(responsePhoneNumber);
+        generalQueryForm.setQueryType(queryType);
+        return generalQueryForm;
+    }
+}
