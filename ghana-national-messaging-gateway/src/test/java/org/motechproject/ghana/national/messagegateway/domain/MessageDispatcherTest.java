7b048b52a6388eec734da00499ff36107c43c8a2
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-03-27 17:25:10 +0530

    Balaji/Sanjana - moved the logic of aggregating sms text to ghana-national

--- ghana-national-messaging-gateway/src/test/java/org/motechproject/ghana/national/messagegateway/domain/MessageDispatcherTest.java
+++ ghana-national-messaging-gateway/src/test/java/org/motechproject/ghana/national/messagegateway/domain/MessageDispatcherTest.java
@@ -8,7 +8,6 @@ import org.motechproject.testing.utils.BaseUnitTest;
 import org.motechproject.util.DateUtil;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -25,30 +24,6 @@ public class MessageDispatcherTest extends BaseUnitTest {
     }
 
     @Test
-    public void shouldAggregateManySMSBasedOnWindowNames() {
-        List<SMS> messagesList = new ArrayList<SMS>() {{
-            Comparator<String> alphabeticalOrder = new Comparator<String>() {
-
-                @Override
-                public int compare(String s, String s1) {
-                    return s.compareTo(s1);
-                }
-            };
-            add(SMS.fromText("window1,milestoneName1,motechId,serialNumber,firstName,lastName", "ph", null, null, alphabeticalOrder));
-            add(SMS.fromText("window1,milestoneName2,motechId,serialNumber,firstName,lastName", "ph", null, null, alphabeticalOrder));
-            add(SMS.fromText("window2,milestoneName,motechId,serialNumber,firstName,lastName", "ph", null, null, alphabeticalOrder));
-            add(SMS.fromText("window2,milestoneName,motechId2,serialNumber,firstName2,lastName3", "ph", null, null, alphabeticalOrder));
-            add(SMS.fromText("window2,milestoneName,motechId3,serialNumber,firstName2,lastName3", "ph", null, null, alphabeticalOrder));
-            add(SMS.fromText("window3,milestoneName,motechId,serialNumber,firstName,lastName", "ph", null, null, alphabeticalOrder));
-        }};
-        assertThat(dispatcher.aggregateSMS(messagesList).getText(),
-                is(equalTo("window1: firstName lastName, motechId, serialNumber, milestoneName1, milestoneName2" +
-                        "%0awindow2: firstName lastName, motechId, serialNumber, milestoneName, firstName2 lastName3, " +
-                        "motechId2, serialNumber, milestoneName, firstName2 lastName3, motechId3, serialNumber, milestoneName" +
-                        "%0awindow3: firstName lastName, motechId, serialNumber, milestoneName")));
-    }
-
-    @Test
     public void shouldCorrelateBasedOnPhoneNumberAndDeliveryDate() {
         final DateTime generationTime = DateUtil.newDateTime(DateUtil.newDate(2000, 1, 1), new Time(10, 10));
         SMS sms = SMS.fromText("text", "ph", generationTime, new DeliveryStrategy() {
@@ -59,7 +34,6 @@ public class MessageDispatcherTest extends BaseUnitTest {
         }, null);
         assertThat(dispatcher.correlateByRecipientAndDeliveryDate(sms), is(equalTo("ph|2000-01-01")));
     }
-
     @Test
     public void shouldReturnIfTheMessageCanBeDispatched() {
         final DateTime generationTime = DateUtil.newDateTime(DateUtil.newDate(2000, 1, 1), new Time(10, 10));
