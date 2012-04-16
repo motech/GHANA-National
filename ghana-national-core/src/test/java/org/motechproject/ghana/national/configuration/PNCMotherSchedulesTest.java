8bdcddf0625a3a89a9cc53b5c446b8b6fba2e2ff
Sanjana Bayya <sanjanb@thoughtworks.com>
2012-04-16 14:23:22 +0530

    Alex,Sanjana|Fixing pnc schedule to accomodate platform change

--- ghana-national-core/src/test/java/org/motechproject/ghana/national/configuration/PNCMotherSchedulesTest.java
+++ ghana-national-core/src/test/java/org/motechproject/ghana/national/configuration/PNCMotherSchedulesTest.java
@@ -45,8 +45,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_2);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("06-Mar-2012", "12:30").toDate()),
-                alert(late, newDateWithTime("08-Mar-2012", "12:30").toDate())
+                alert(earliest, newDateWithTime("06-Mar-2012", "12:29").toDate()),
+                alert(late, newDateWithTime("08-Mar-2012", "12:29").toDate())
         ));
     }
 
@@ -57,8 +57,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_3);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("13-Mar-2012", "12:30").toDate()),
-                alert(late, newDateWithTime("15-Mar-2012", "12:30").toDate())
+                alert(earliest, newDateWithTime("13-Mar-2012", "12:29").toDate()),
+                alert(late, newDateWithTime("15-Mar-2012", "12:29").toDate())
         ));
     }
     //////////////////////////////////////////// Case: Mother's Delivery is registered >= 6 hours from child birth ////////////////////////////////////////////////////////
@@ -80,8 +80,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_2);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("08-Jan-2012", "12:30").toDate()),
-                alert(late, newDateWithTime("10-Jan-2012", "12:30").toDate())
+                alert(earliest, newDateWithTime("08-Jan-2012", "12:29").toDate()),
+                alert(late, newDateWithTime("10-Jan-2012", "12:29").toDate())
         ));
     }
 
@@ -92,8 +92,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_3);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("15-Jan-2012", "12:30").toDate()),
-                alert(late, newDateWithTime("17-Jan-2012", "12:30").toDate())
+                alert(earliest, newDateWithTime("15-Jan-2012", "12:29").toDate()),
+                alert(late, newDateWithTime("17-Jan-2012", "12:29").toDate())
         ));
     }
     //////////////////////////////////////////// Case: Mother's Delivery is registered >= 12 hours from child birth ////////////////////////////////////////////////////////
@@ -113,8 +113,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_2);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("18-Apr-2012", "4:30").toDate()),
-                alert(late, newDateWithTime("20-Apr-2012", "4:30").toDate())
+                alert(earliest, newDateWithTime("18-Apr-2012", "4:29").toDate()),
+                alert(late, newDateWithTime("20-Apr-2012", "4:29").toDate())
         ));
     }
 
@@ -125,8 +125,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_3);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("25-Apr-2012", "4:30").toDate()),
-                alert(late, newDateWithTime("27-Apr-2012", "4:30").toDate())
+                alert(earliest, newDateWithTime("25-Apr-2012", "4:29").toDate()),
+                alert(late, newDateWithTime("27-Apr-2012", "4:29").toDate())
         ));
     }
     //////////////////////////////////////////// Case: Mother's Delivery is registered >= 48 hours from child birth ////////////////////////////////////////////////////////
@@ -146,8 +146,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_2);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("01-Jun-2012", "17:04").toDate()),
-                alert(late, newDateWithTime("03-Jun-2012", "17:04").toDate())
+                alert(earliest, newDateWithTime("01-Jun-2012", "17:03").toDate()),
+                alert(late, newDateWithTime("03-Jun-2012", "17:03").toDate())
         ));
     }
 
@@ -158,8 +158,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_3);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("08-Jun-2012", "17:04").toDate()),
-                alert(late, newDateWithTime("10-Jun-2012", "17:04").toDate())
+                alert(earliest, newDateWithTime("08-Jun-2012", "17:03").toDate()),
+                alert(late, newDateWithTime("10-Jun-2012", "17:03").toDate())
         ));
     }
 
@@ -180,7 +180,7 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_2);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(late, newDateWithTime("22-Mar-2012", "09:45").toDate())
+                alert(late, newDateWithTime("22-Mar-2012", "09:44").toDate())
         ));
     }
 
@@ -191,8 +191,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
 
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_MOTHER_3);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
-                alert(earliest, newDateWithTime("27-Mar-2012", "09:45").toDate()),
-                alert(late, newDateWithTime("29-Mar-2012", "09:45").toDate())
+                alert(earliest, newDateWithTime("27-Mar-2012", "09:44").toDate()),
+                alert(late, newDateWithTime("29-Mar-2012", "09:44").toDate())
         ));
     }
 
@@ -223,8 +223,8 @@ public class PNCMotherSchedulesTest extends BaseScheduleTrackingTest {
         enrollmentId = scheduleAlertForPNCMother(dateOfBirth, PNC_CHILD_3);
         assertTestAlerts(captureAlertsForNextMilestone(enrollmentId), asList(
                 // seconds are not considered
-                alert(earliest, newDateWithTime("11-Feb-2012", "14:25").toDate()),
-                alert(late, newDateWithTime("13-Feb-2012", "14:25").toDate())
+                alert(earliest, newDateWithTime("11-Feb-2012", "14:24").toDate()),
+                alert(late, newDateWithTime("13-Feb-2012", "14:24").toDate())
         ));
     }
     //////////////////////////////////////////// Case: Mother's Delivery is registered over 14 days of age of child- PNC1 is NA ////////////////////////////////////////////////////////
