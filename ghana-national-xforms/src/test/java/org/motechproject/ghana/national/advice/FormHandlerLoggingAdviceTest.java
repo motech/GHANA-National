6441292d8a937977a2821358c6645cda9a0f9d80
sdeepan <deepans@thoughtworks.com:q!>
2012-05-28 14:20:15 +0530

    Deepan|Fixed logging advice tests

--- ghana-national-xforms/src/test/java/org/motechproject/ghana/national/advice/FormHandlerLoggingAdviceTest.java
+++ ghana-national-xforms/src/test/java/org/motechproject/ghana/national/advice/FormHandlerLoggingAdviceTest.java
@@ -3,17 +3,18 @@ package org.motechproject.ghana.national.advice;
 import org.joda.time.DateTime;
 import org.joda.time.Period;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.motechproject.ghana.national.bean.ClientDeathForm;
+import org.motechproject.ghana.national.bean.RegisterClientForm;
 import org.motechproject.ghana.national.domain.Constants;
 import org.motechproject.ghana.national.domain.Facility;
 import org.motechproject.ghana.national.domain.Patient;
 import org.motechproject.ghana.national.handler.CareScheduleHandler;
 import org.motechproject.ghana.national.handlers.ClientDeathFormHandler;
+import org.motechproject.ghana.national.handlers.PatientRegistrationFormHandler;
 import org.motechproject.ghana.national.logger.advice.FormHandlerLoggingAdvice;
 import org.motechproject.ghana.national.repository.AllObservations;
 import org.motechproject.ghana.national.repository.SMSGateway;
@@ -21,6 +22,8 @@ import org.motechproject.ghana.national.service.FacilityService;
 import org.motechproject.ghana.national.service.MobileMidwifeService;
 import org.motechproject.ghana.national.service.PatientService;
 import org.motechproject.mobileforms.api.callbacks.FormPublishHandler;
+import org.motechproject.mobileforms.api.domain.FormBean;
+import org.motechproject.mobileforms.api.domain.FormBeanGroup;
 import org.motechproject.model.MotechEvent;
 import org.motechproject.mrs.model.MRSFacility;
 import org.motechproject.mrs.model.MRSPatient;
@@ -37,6 +40,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.mockito.Matchers.anyString;
@@ -56,8 +60,7 @@ public class FormHandlerLoggingAdviceTest {
     private FacilityService facilityService;
 
     @Mock
-    private MobileMidwifeService mobileMidwifeService;
-
+    private PatientRegistrationFormHandler patientRegistrationFormHandler;
     @Mock
     private AllObservations allObservations;
 
@@ -71,8 +74,8 @@ public class FormHandlerLoggingAdviceTest {
     private FormHandlerLoggingAdvice formHandlerLoggingAdvice;
 
     @Autowired
-    @Qualifier("clientDeathFormHandler")
-    private FormPublishHandler clientDeathFormHandler;
+    @Qualifier("mobileFormHandler")
+    private FormPublishHandler formPublishHandler;
 
     @Autowired
     private CareScheduleHandler careScheduleHandler;
@@ -89,19 +92,18 @@ public class FormHandlerLoggingAdviceTest {
     }
 
     @Test
-    @Ignore
-    public void shouldLog(){
-        final ClientDeathForm clientDeathForm = new ClientDeathForm();
-        clientDeathFormHandler.handleFormEvent(new MotechEvent("subject", new HashMap<String, Object>() {{
-            put(Constants.FORM_BEAN, clientDeathForm);
+    public void shouldLog() {
+        final RegisterClientForm registerClientForm = new RegisterClientForm();
+        formPublishHandler.handleFormEvent(new MotechEvent("subject", new HashMap<String, Object>() {{
+            put("formBeanGroup", new FormBeanGroup(Arrays.<FormBean>asList(registerClientForm)));
         }}));
         MilestoneAlert milestoneAlert = MilestoneAlert.fromMilestone(new Milestone("milestone", Period.ZERO, Period.days(1), Period.hours(12), Period.months(1)), DateTime.now());
         careScheduleHandler.handlePregnancyAlert(new MilestoneEvent(null, null, milestoneAlert, WindowName.earliest.name(), null));
         verify(jdbcTemplate, times(2)).execute(anyString());
     }
 
-    private void mockCareScheduleHandler() throws Exception{
-        final CareScheduleHandler proxiedCareScheduleHandler = (CareScheduleHandler)((Advised) careScheduleHandler).getTargetSource().getTarget();
+    private void mockCareScheduleHandler() throws Exception {
+        final CareScheduleHandler proxiedCareScheduleHandler = (CareScheduleHandler) ((Advised) careScheduleHandler).getTargetSource().getTarget();
         ReflectionTestUtils.setField(proxiedCareScheduleHandler, "patientService", patientService);
         ReflectionTestUtils.setField(proxiedCareScheduleHandler, "facilityService", facilityService);
         ReflectionTestUtils.setField(proxiedCareScheduleHandler, "allObservations", allObservations);
@@ -109,9 +111,7 @@ public class FormHandlerLoggingAdviceTest {
     }
 
     private void mockFormPublishHandler() throws Exception {
-        final ClientDeathFormHandler proxiedClientDeathFormHandler = (ClientDeathFormHandler)((Advised) clientDeathFormHandler).getTargetSource().getTarget();
-        ReflectionTestUtils.setField(proxiedClientDeathFormHandler, "mobileMidwifeService", mobileMidwifeService);
-        ReflectionTestUtils.setField(proxiedClientDeathFormHandler, "patientService", patientService);
-
+        final FormPublishHandler proxiedClientDeathFormHandler = (FormPublishHandler) ((Advised) formPublishHandler).getTargetSource().getTarget();
+        ReflectionTestUtils.setField(proxiedClientDeathFormHandler, "patientRegistrationFormHandler", patientRegistrationFormHandler);
     }
 }
