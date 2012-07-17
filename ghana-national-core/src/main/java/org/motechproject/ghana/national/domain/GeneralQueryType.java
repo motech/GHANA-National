42f6010c125faae373c5e7259eec24c4b30f2639
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2012-07-17 11:04:18 +0530

    <Aus> added pneumococcal and rotavirus to Defaulter list

--- ghana-national-core/src/main/java/org/motechproject/ghana/national/domain/GeneralQueryType.java
+++ ghana-national-core/src/main/java/org/motechproject/ghana/national/domain/GeneralQueryType.java
@@ -6,19 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.ANC_DELIVERY;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.ANC_IPT_VACCINE;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.CWC_IPT_VACCINE;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.CWC_OPV_0;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.CWC_OPV_OTHERS;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.CWC_PENTA;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.PNC_CHILD_1;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.PNC_CHILD_2;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.PNC_CHILD_3;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.PNC_MOTHER_1;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.PNC_MOTHER_2;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.PNC_MOTHER_3;
-import static org.motechproject.ghana.national.configuration.ScheduleNames.TT_VACCINATION;
+import static org.motechproject.ghana.national.configuration.ScheduleNames.*;
 
 public enum GeneralQueryType {
     OVERDUE_DELIVERIES("Overdue Defaulters",asList(ANC_DELIVERY)),
@@ -28,7 +16,7 @@ public enum GeneralQueryType {
     TT_DEFAULTERS("TT Defaulters",asList(TT_VACCINATION)),
     PNC_C_DEFAULTERS("PNC Child Defaulters",asList(PNC_CHILD_1, PNC_CHILD_2, PNC_CHILD_3)),
     PNC_M_DEFAULTERS("PNC Mother Defaulters",asList(PNC_MOTHER_1, PNC_MOTHER_2, PNC_MOTHER_3)),
-    CWC_DEFAULTERS("CWC Defaulters",asList(CWC_IPT_VACCINE, CWC_OPV_0, CWC_OPV_OTHERS, CWC_PENTA));
+    CWC_DEFAULTERS("CWC Defaulters",asList(CWC_IPT_VACCINE, CWC_OPV_0, CWC_OPV_OTHERS, CWC_PENTA, CWC_PNEUMOCOCCAL, CWC_ROTAVIRUS));
 
     private String name;
     private List<ScheduleNames> scheduleNames;
