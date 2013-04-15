eea0a0d847ebba1a48a6f5f4325c9c61367e1623
Freduah <freduahamponsah@gmail.com>
2013-04-15 12:52:33 -0400

    added [Growth monitoring (% and date)] to CWCVisit form added [VDRL treatment [Y, N, N/A]], HIV treatment [Y or N], Gestation age, Counseled on FP [Y or N] and Births [live, fresh still birth, macerated still birth] to ANCVisit form.

--- ghana-national-xforms/src/main/java/org/motechproject/ghana/national/bean/CWCVisitForm.java
+++ ghana-national-xforms/src/main/java/org/motechproject/ghana/national/bean/CWCVisitForm.java
@@ -1,6 +1,7 @@
 package org.motechproject.ghana.national.bean;
 
 import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.ser.ArraySerializers;
 import org.motechproject.ghana.national.validator.field.MotechId;
 import org.motechproject.mobileforms.api.domain.FormBean;
 import org.motechproject.mobileforms.api.validator.annotations.MaxLength;
@@ -57,6 +58,27 @@ public class CWCVisitForm extends FormBean {
 
     private String comments;
     private String measlesdose;
+
+    private Doubble growthmonitoringpercentage;
+    private Date    growthmonitoringdate;
+
+
+    public Doubble getGrowthmonitoringpercentage() {
+        return growthmonitoringpercentage;
+    }
+
+    public void setGrowthmonitoringpercentage(Doubble growthmonitoringpercentage) {
+        this.growthmonitoringpercentage = growthmonitoringpercentage;
+    }
+
+    public Date getGrowthmonitoringdate() {
+        return growthmonitoringdate;
+    }
+
+    public void setGrowthmonitoringdate(Date growthmonitoringdate) {
+        this.growthmonitoringdate = growthmonitoringdate;
+    }
+
 
     public String getStaffId() {
         return staffId;
