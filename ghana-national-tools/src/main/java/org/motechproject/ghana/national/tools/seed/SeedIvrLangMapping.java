cdde890e9093fbb2ce37dbb8d85ac26ae3b1c2a4
Freduah <freduahamponsah@gmail.com>
2013-07-12 10:16:19 -0400

    Added New Class SeedIvrLanagMapping

--- ghana-national-tools/src/main/java/org/motechproject/ghana/national/tools/seed/SeedIvrLangMapping.java
+++ ghana-national-tools/src/main/java/org/motechproject/ghana/national/tools/seed/SeedIvrLangMapping.java
@@ -3,12 +3,13 @@ package org.motechproject.ghana.national.tools.seed;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class SeedAudioFiles {
+public class SeedIvrLangMapping {
+
     public static final String APPLICATION_CONTEXT_XML = "applicationContext-tools.xml";
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML);
-        SeedLoader seedLoader = (SeedLoader) context.getBean("seedAudioFiles");
+        SeedLoader seedLoader = (SeedLoader) context.getBean("ivrLangMapping");
         seedLoader.load();
     }
 }
